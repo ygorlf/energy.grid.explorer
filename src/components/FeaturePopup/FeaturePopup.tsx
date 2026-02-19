1a547f28c36596e3be2c070172bbef723a8233de
Ygor Fonseca <ygor.luiz.fonseca@gmail.com>
2026-02-19 17:34:19 -0300

    feat: add substation click popup

---
+++ src/components/FeaturePopup/FeaturePopup.tsx
@@ -0,0 +1,5 @@
+// Note: Popups are rendered via MapLibre's Popup API in MapView.tsx
+// This file holds the popup CSS that MapLibre injects into the DOM.
+// If we need React-rendered popups in the future, implement them here.
+
+export {};
